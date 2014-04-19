c26c42fc5cc4fee1583406fc9ac4af6202c23ecf
Bryan Gamotea <bryan.gamotea26@gmail.com>
2014-04-19 18:59:42 +0800

    initial commit

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,71 +1,59 @@
 $(document).ready(function(){
-
-
-
-	/* 
- * Lazy Line Painter - Path Object 
- * Generated using 'SVG to Lazy Line Converter'
- * 
- * http://lazylinepainter.info 
- * Copyright 2013, Cam O'Connell  
- *  
- */ 
- 
-var pathObj = {
-    "curve-line-two": {
+  console.log($(document).scrollTop());
+  function drawLineOne () {
+      var pathObj = {
+      "curve-line-1": {
         "strokepath": [
-            {
-                "path": "M207.976,224.89l0.015-0.999c0.107,0.001,43.107,0.631,86.083,26.32     c21.487,12.844,42.968,31.955,59.079,60.383C369.264,339.023,380,376.77,380,426.89h-1c0-45.556-8.99-84.517-26.717-115.802     c-14.17-25.002-33.926-45.195-58.724-60.018c-42.279-25.273-85.146-26.173-85.572-26.18H207.976",
-                "duration": 600
-            },
-            {
-                "path": "M379,426.89h1c0,89.938-24.725,137.824-52.006,163.285c-33.496,31.268-70.845,28.726-70.994,28.715     c-0.003,0-43.096,0.788-50.999,0l0.098-0.995c7.755,0.772,50.452,0.003,50.97-0.002c0.37,0.025,37.411,2.199,70.243-28.446     C361.609,557.431,379,502.742,379,426.89",
-                "duration": 600
-            }
-        ],
-        "dimensions": {
-            "width": 596,
-            "height": 842
+          {
+            "path": "M207.976,224.89l0.016-0.999c0.106,0.001,43.106,0.631,86.083,26.32      c21.486,12.844,42.967,31.955,59.078,60.383C369.264,339.023,380,376.77,380,426.89h-1c0-45.556-8.99-84.517-26.718-115.802      c-14.17-25.002-33.926-45.195-58.724-60.018c-42.279-25.273-85.146-26.173-85.572-26.18H207.976",
+            "duration": 3000
+          },
+          {
+            "path": "M379,426.89h1c0,89.938-24.726,137.824-52.007,163.285c-33.495,31.268-70.845,28.727-70.993,28.715      c-0.003,0-43.097,0.787-50.999,0l0.098-0.994c7.755,0.771,50.452,0.002,50.97-0.004c0.37,0.025,37.411,2.199,70.243-28.445      C361.609,557.431,379,502.742,379,426.89",
+            "duration": 3000
         }
+      ],
+      "dimensions": {
+        "width": 596,
+        "height": 842
+        }
+      }
     }
-}; 
- 
- 
-/* 
- Setup and Paint your lazyline! 
- */ 
- 
- $(document).ready(function(){ 
- $('#curve-line-two').lazylinepainter( 
- {
-    "svgData": pathObj,
-    "strokeWidth": 2,
-    "strokeColor": "#e09b99"
-}).lazylinepainter('paint'); 
- });	
 
 
+    $('#curve-line-1').lazylinepainter( 
+     {
+        "svgData": pathObj,
+        "strokeWidth": 1,
+        "strokeColor": "#0bb3ff"
+    }).lazylinepainter('paint'); 
+  } 
 
+  function drawLineTwo () {
+    var pathObj = {
+      "curve-line-2": {
+          "strokepath": [
+              {
+                  "path": "M460,33.89v1h-46c-87.25-0.803-138.355,25.127-168.25,56.901      c-46.174,49.077-41.749,112.099-41.749,112.099v381.998V742.89c0,0-1.933,32.766,14.937,50.658      c6.337,6.721,15.324,11.342,28.063,11.342v0.998c-11.812,0-21.498-3.92-28.79-11.652c-16.97-18-15.29-50.051-15.212-51.346      V585.888l0.004-381.929c-0.046-0.634-3.925-64.022,42.019-112.853C281.398,52.444,338.252,33.193,414,33.89H460",
+                  "duration": 4000
+              }
+          ],
+          "dimensions": {
+              "width": 596,
+              "height": 842
+          }
+      }
+    }; 
+   
+   
+   $('#curve-line-2').lazylinepainter( 
+     {
+      "svgData": pathObj,
+      "strokeWidth": 1,
+      "strokeColor": "#0bb3ff"
+    }).lazylinepainter('paint'); 
 
-
-	// end of arrow one
-
-	var myVideo = $('.video');
-	if (typeof myVideo.loop == 'boolean') { // loop supported
-	    myVideo.loop = true;
-	} else { // loop property not supported
-	    myVideo.on('ended', function () {
-	    this.currentTime = 0;
-	    this.play();
-	    }, false);
-	}
-	$('.video').get(0).play();
-
-
-	$(window).bind('scroll', function(e){
-		parallaxScroll();
-	});
-
+  }
 
 	function commaSeparateNumber(val){
     while (/(\d+)(\d{3})/.test(val.toString())){
@@ -75,14 +63,14 @@ var pathObj = {
   }
 
 	function parallaxScroll(){
-		// var scrolledY = $(window).scrollTop();
+		var scrolledY = $(window).scrollTop();
 		// $('.bg-1').css('top','-'+((scrolledY*0.75))+'px');
 		// $('.bg-2').css('top','-'+((scrolledY*1))+'px');
 
 		if(scrolledY < 400) {
 			$('.one').addClass('active');
 			$('.two').removeClass('active');
-		} else if (scrolledY >= 400) {
+		} else if (scrolledY > 400) {
 				$('.one').removeClass('active');
 				$('.two').addClass('active');
 				$('.blue-circle').fadeIn(1000);
@@ -97,6 +85,15 @@ var pathObj = {
 			  	$('.number').html(commaSeparateNumber(this.spanValue));
 			  }
 			});
-		}
+        drawLineOne();
+        drawLineTwo();
+		  }
+       
+
+
 	}
+
+    $(window).bind('scroll', function(e){
+     parallaxScroll();
+    });
 });
